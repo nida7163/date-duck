cc31adc0bf2ef301ccd89da6a5d7a8756a40ccd0
Andrew Coleman <Andrew@lawn-128-61-127-169.lawn.gatech.edu>
2017-06-22 20:40:15 -0400

    added user location functionality

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+$( document ).ready(function() {
 
+	var userZipCode = "";
 
 	$("#zip-submit").click(function(){
-	  var userZipCode = $("#zip").val();
-	  var wuAPIKey = "a4c27a2f36ce4003";
-	  var queryURL = "http://api.wunderground.com/api/" + wuAPIKey + "/conditions/q/30076.json";
+		event.preventDefault();
+		userZipCode = $("#zip").val();
+		console.log(userZipCode);
+		$("#get-started").css("display", "none")
+		$("#vibe-selection").show(300);
 
-	  $.ajax({
-	    url: queryURL,
-	    method: "GET"
-	  })
+		var wuAPIKey = "a4c27a2f36ce4003";
+		var queryURL = "http://api.wunderground.com/api/" + wuAPIKey + "/conditions/q/" + userZipCode + ".json";
+
+		$.ajax({
+	    	url: queryURL,
+	    	method: "GET"
+	  	})
 	      
 	  // retrieved data "response"
 	  .done(function(response) {
@@ -17,151 +24,163 @@
 	    console.log("Current weather: " + response.current_observation.weather);
 	    console.log("Current temperature: " + response.current_observation.temp_f + " degrees(f)")
 
-	    $("#conditions").html("Weather conditions: " + response.current_observation.weather);
-	    $("#temperature").html("Temperature: " + response.current_observation.temp_f + " degrees(f)");
+	    $("#conditions").html("<span class='conditions-heading'>Conditions:</span><br><span class='conditions-response'>" + response.current_observation.weather) + "</span>";
+	    $("#temperature").html("<span class='temperature-heading'>Temperature:</span><br><span class='temperature-response'>" + response.current_observation.temp_f + " F</span");
 	  });
   	});
 
 
 
 
-var app = {};
+		var app = {};
 
-var clientId = 'JFYFDNW4QYLUK5AKHG2LLQBJ2OMGE1J24X0NUHJTED0EQT4H'
-var clientSecret = 'T5TVNZ4L3X0M3XFXURPGSA040NWBR4MCQG1VU05PWEI1ASZA'
+		var clientId = 'JFYFDNW4QYLUK5AKHG2LLQBJ2OMGE1J24X0NUHJTED0EQT4H'
+		var clientSecret = 'T5TVNZ4L3X0M3XFXURPGSA040NWBR4MCQG1VU05PWEI1ASZA'
 
-app.romantic = function(query) {
-	$.ajax({
-		url: 'https://api.foursquare.com/v2/venues/search', 
-		type: 'GET',
-		dataType: 'jsonp',
-		data: {
-			client_id: clientId,
-			client_secret: clientSecret,
-			intent: 'browse',
-			v: 20150513,
-			format: 'jsonp', 
-			near: 'Atlanta, GA',
-			query: query, 
-			limit: 6,
-			openNow : true,
-			sortByDistance: 1,
-			venuePhotos : 1
-		}, 
-		success: function(data) {
-			app.dataIwant = data.response.venues;
-			app.displayRomantic(app.dataIwant);
-		}
-	});
-};
-
-
-app.displayRomantic = function(dateRomantic) {
-	$('#dateNightResults').empty();
-	$.each(dateRomantic, function(item, venue){
-		// var photoSize = '500x500';
-		// var photoPrefix = venue.photos.groups[0].items[0];
-		var $venueTitle = '<h2>' + venue.name + '</h2>';
-		var $venueLocation = '<p>' + venue.location.address + '</p>';
-		// var $venuePhotos = $('<img>').attr('src', photoPrefix.prefix + photoPrefix.photoSize + photoPrefix.suffix);
-		var $venueContainer = $('<div>').addClass('dateContainer').append($venueTitle, $venueLocation);
-
-		$('#dateNightResults').append($venueContainer);
-	});
-
-}
+		app.romantic = function(query) {
+			$.ajax({
+				url: 'https://api.foursquare.com/v2/venues/search', 
+				type: 'GET',
+				dataType: 'jsonp',
+				data: {
+					client_id: clientId,
+					client_secret: clientSecret,
+					intent: 'browse',
+					v: 20150513,
+					format: 'jsonp', 
+					near: userZipCode,
+					query: query, 
+					limit: 6,
+					openNow : true,
+					sortByDistance: 1,
+					venuePhotos : 1
+				}, 
+				success: function(data) {
+					app.dataIwant = data.response.venues;
+					app.displayRomantic(app.dataIwant);
+					console.log("Foursquare API Object:")
+					console.log(data.response)
+				}
+			});
+		};
 
 
-// var $venuePhotos = $('<img>').attr('src', item.venue.photos.groups[0].items[0].prefix + photoSize + item.venue.photos.groups[0].items[0].suffix);
+		app.displayRomantic = function(dateRomantic) {
+			$('#dateNightResults').empty();
+			$.each(dateRomantic, function(item, venue){
+				// var photoSize = '500x500';
+				// var photoPrefix = venue.photos.groups[0].items[0];
+				var $venueTitle = '<h2>' + venue.name + '</h2>';
+				var $venueLocation = '<p>' + venue.location.address + '</p>';
+				// var $venuePhotos = $('<img>').attr('src', photoPrefix.prefix + photoPrefix.photoSize + photoPrefix.suffix);
+				var $venueContainer = $('<div>').addClass('dateContainer').append($venueTitle, $venueLocation);
 
-//OR
-
-//venue photo
-		// var photoPrefix = places[i].venue.photos.groups[0].items[0];
-		// var photo = $('<img>').attr('src',photoPrefix.prefix + photoPrefix.height + photoPrefix.suffix);
-
-
-
-	// Using dateType need to get a random number and select a random element from date type
-	// With random date we need to display part 1, part 2, part 3
-	// With those displayed, we then need to allow user to click on an icon(part) and make a call to Foursquare api to get information
-	// With this information we need to display it on the page
-
-app.events = function(){
-
-	$('.vibes li').on('click', function() {
-		var getDates = $(this).text();
-		
-
-		if(getDates==='Romantic'){
-			//display a random romanticDate
-			app.displayDate(romanticDates);
+				$('#dateNightResults').append($venueContainer);
+			});
 
 		}
-		if(getDates==='Fun'){
-			//display a random classicDate
-			app.displayDate(funDates);
+
+
+		// var $venuePhotos = $('<img>').attr('src', item.venue.photos.groups[0].items[0].prefix + photoSize + item.venue.photos.groups[0].items[0].suffix);
+
+		//OR
+
+		//venue photo
+				// var photoPrefix = places[i].venue.photos.groups[0].items[0];
+				// var photo = $('<img>').attr('src',photoPrefix.prefix + photoPrefix.height + photoPrefix.suffix);
+
+
+
+			// Using dateType need to get a random number and select a random element from date type
+			// With random date we need to display part 1, part 2, part 3
+			// With those displayed, we then need to allow user to click on an icon(part) and make a call to Foursquare api to get information
+			// With this information we need to display it on the page
+
+		app.events = function(){
+
+			$('.vibes li').on('click', function() {
+				
+				$("#duck-icon").animate({height: "40px", width: "40px"});
+				$("#page-title").hide(300);
+
+
+				var getDates = $(this).text();
+				
+
+				if(getDates==='Romantic'){
+					//display a random romanticDate
+					app.displayDate(romanticDates);
+
+				}
+				if(getDates==='Fun'){
+					//display a random classicDate
+					app.displayDate(funDates);
+				}
+				if(getDates==='Sporty'){
+					//display a random sportyDate
+					app.displayDate(sportyDates);
+				}
+				if(getDates==='Chill'){
+					//display a random chillDate
+					app.displayDate(chillDates);
+				}
+				if(getDates==='Extreme'){
+					//display a random extremeDate
+					app.displayDate(extremeDates);
+				}
+
+				app.dateRandomizer(getDates);
+			});
+
+		};
+
+		app.randomNumber = function(n) {
+			return (Math.floor(Math.random() * n) );
+		};
+
+		app.dateRandomizer = function(datesArray) {
+			var randomDateIndex = app.randomNumber(datesArray.length)
+			var randomDate = datesArray[randomDateIndex];
+			return randomDate;
+		};
+
+
+		app.displayDate = function(dateType){
+
+			var myDate = app.dateRandomizer(dateType)
+			
+
+			var firstIdea = '<p>' + myDate.part1.title + '</p>';
+			var firstImg = '<img src="' + myDate.part1.icon + '"/>';
+			var firstDiv = '<div class="first dateIdeas" data-date="' + myDate.part1.query + '">' + firstImg + firstIdea  + '</div>';
+
+			var secondIdea = '<p>' + myDate.part2.title + '</p>';
+			var secondImg = '<img src="' + myDate.part2.icon + '"/>';
+			var secondDiv = '<div class="second dateIdeas" data-date="' + myDate.part2.query + '">' + secondImg + secondIdea  + '</div>';
+
+			var thirdIdea = '<p>' + myDate.part3.title + '</p>';
+			var thirdImg = '<img src="' + myDate.part3.icon + '"/>';
+			var thirdDiv = '<div class="third dateIdeas" data-date="' + myDate.part3.query + '">' + thirdImg+ thirdIdea  + '</div>';
+
+			$('.dateOptions').empty();
+			$('.dateOptions').append(firstDiv, secondDiv, thirdDiv);
+			app.searchDates();
+			
 		}
-		if(getDates==='Sporty'){
-			//display a random sportyDate
-			app.displayDate(sportyDates);
+
+		app.searchDates = function() {
+
+
+			$('.dateOptions').on('click', '.dateIdeas', function() {
+				$(".dateOptions").animate({height: "50%", width: "50%"});
+				$("#weather").slideDown("slow");
+
+
+				var idea = $(this).data('date');
+				app.romantic(idea);
+			});
+
 		}
-		if(getDates==='Chill'){
-			//display a random chillDate
-			app.displayDate(chillDates);
-		}
-		if(getDates==='Extreme'){
-			//display a random extremeDate
-			app.displayDate(extremeDates);
-		}
-
-		app.dateRandomizer(getDates);
-	});
-
-};
-
-app.randomNumber = function(n) {
-	return (Math.floor(Math.random() * n) );
-};
-
-app.dateRandomizer = function(datesArray) {
-	var randomDateIndex = app.randomNumber(datesArray.length)
-	var randomDate = datesArray[randomDateIndex];
-	return randomDate;
-};
-
-
-app.displayDate = function(dateType){
-
-	var myDate = app.dateRandomizer(dateType)
-	
-
-	var firstIdea = '<p>' + myDate.part1.title + '</p>';
-	var firstImg = '<img src="' + myDate.part1.icon + '"/>';
-	var firstDiv = '<div class="first dateIdeas" data-date="' + myDate.part1.query + '">' + firstImg + firstIdea  + '</div>';
-
-	var secondIdea = '<p>' + myDate.part2.title + '</p>';
-	var secondImg = '<img src="' + myDate.part2.icon + '"/>';
-	var secondDiv = '<div class="second dateIdeas" data-date="' + myDate.part2.query + '">' + secondImg + secondIdea  + '</div>';
-
-	var thirdIdea = '<p>' + myDate.part3.title + '</p>';
-	var thirdImg = '<img src="' + myDate.part3.icon + '"/>';
-	var thirdDiv = '<div class="third dateIdeas" data-date="' + myDate.part3.query + '">' + thirdImg+ thirdIdea  + '</div>';
-
-	$('.dateOptions').empty();
-	$('.dateOptions').append(firstDiv, secondDiv, thirdDiv);
-	app.searchDates();
-	
-}
-
-app.searchDates = function() {
-
-	$('.dateOptions').on('click', '.dateIdeas', function() {
-		var idea = $(this).data('date');
-		app.romantic(idea);
-	});
-
-}
 
 
 
@@ -468,4 +487,7 @@ app.init = function() {
 
 $(function() {
 	app.init()
+});
+
+// end of .ready() method
 });
